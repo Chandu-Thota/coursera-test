b5ff8f91d01f0e152b0900beb37ca4cddb9a796a
Chandu-Thota <chanduthota2429@gmail.com>
2020-07-01 11:58:48 +0530

    Name Calculator

--- app.js
+++ app.js
@@ -4,8 +4,22 @@
 angular.module('NameCalculator',[])
 
 .controller('NameCalculatorController', function ($scope) {
+	$scope.name = "";
+	$scope.totalvalue = 0;
 	
-})
+	$scope.displaynumeric = function () {
+		var totalnamevalue =calnumforstring($scope.name);
+		$scope.totalvalue = totalnamevalue;
+	};
+
+	function calnumforstring(string){
+		var totalstrvalue = 0;
+		for (var i =0; i < string.length; i++) {
+			totalstrvalue += string.charCodeAt(i);
+		}
+		return totalstrvalue;
+	}
+});
 
 
 
